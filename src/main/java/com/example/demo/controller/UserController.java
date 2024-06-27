95909c053c67ff09189e8eebc93201a702d92bcb
stephen <stephenola21@gmail.com>
2024-06-27 14:59:43 +0100

    implemented get one user

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +16,26 @@ import com.example.demo.repository.UserRepository;
 
 // @Controller // This means that this class is a Controller
 @RestController
-@RequestMapping(path="/api") // This means URL's start with /demo (after Application path)
+@RequestMapping(path = "/api") // This means URL's start with /demo (after Application path)
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping(path="/allusers")
+    @GetMapping(path = "/allusers")
     public @ResponseBody List<User> getAllUsers() {
-      // This returns a JSON or XML with the users
-      return userRepository.findAll();
+        // This returns a JSON or XML with the users
+        return userRepository.findAll();
     }
-    
+
+    @GetMapping(path = "/user/{userId}")
+    public ResponseEntity<User> getOneUser(@PathVariable Long userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            return ResponseEntity.ok(userOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
